c04273a331259dc61bc977d51c5ca65ca0cef20d
adrisandoval2006 <A01667445@tec.mxm>
2025-09-18 13:36:17 -0600

    Se mueve raro

--- snake.py
+++ snake.py
@@ -1,28 +1,32 @@
-"""Snake, classic arcade game"""
-
-from random import randrange
 from turtle import *
-
+from random import randrange, choice
 from freegames import square, vector
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
-
 def change(x, y):
-    """Change snake direction."""
+    "Change snake direction."
     aim.x = x
     aim.y = y
 
+def inside(point):
+    "Return True if point inside boundaries."
+    return -200 < point.x < 190 and -200 < point.y < 190
 
-def inside(head):
-    """Return True if head inside boundaries."""
-    return -200 < head.x < 190 and -200 < head.y < 190
+def move_food():
+    "Move food randomly one step but keep inside the window."
+    directions = [vector(10, 0), vector(-10, 0),
+                  vector(0, 10), vector(0, -10)]
+    step = choice(directions) 
+    new_food = food + step    
 
+    if inside(new_food):      
+        food.move(step)       
 
 def move():
-    """Move snake forward one segment."""
+    "Move snake forward one segment."
     head = snake[-1].copy()
     head.move(aim)
 
@@ -40,6 +44,8 @@ def move():
     else:
         snake.pop(0)
 
+    move_food()
+
     clear()
 
     for body in snake:
@@ -48,7 +54,6 @@ def move():
     square(food.x, food.y, 9, 'green')
     update()
     ontimer(move, 100)
-
 
 setup(420, 420, 370, 0)
 hideturtle()
@@ -60,3 +65,4 @@ onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
 move()
 done()
+
